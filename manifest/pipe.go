bf001747d6654c94653ff1f22691bcb2bac2fc66
Shantanu Raj <s@sraj.me>
2023-09-16 15:33:55 +0200

    fix(pipe): Don't use custom io.Reader

--- manifest/pipe.go
+++ manifest/pipe.go
@@ -2,7 +2,6 @@ package manifest
 
 import (
 	"context"
-	"io"
 	"io/fs"
 	"strings"
 
@@ -22,21 +21,8 @@ func (p Pipe) Command(ctx context.Context, execDir fs.FS, input *string) (*Cmd, 
 
 	cmd, cleanup := newCommand(ctx, execDir, "bash", "-c", p.Exec+" "+strings.Join(args, " "))
 	if p.Stdin != nil {
-		cmd.Stdin = &inputReader{input: p.Stdin.Value(input)}
+		in := strings.Join(p.Stdin.Value(input), "\n")
+		cmd.Stdin = strings.NewReader(in)
 	}
 	return cmd, cleanup
-}
-
-type inputReader struct {
-	input []string
-	idx   int
-}
-
-func (r *inputReader) Read(p []byte) (int, error) {
-	if r.idx >= len(r.input) {
-		return 0, io.EOF
-	}
-	n := copy(p, r.input[r.idx])
-	r.idx++
-	return n, nil
 }
